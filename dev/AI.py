0a37799b28bbff0d0f1a8219453b4ec27a4e8cbe
saarioka <34010391+saarioka@users.noreply.github.com>
2018-02-25 23:38:15 +0200

    whatsapp convos

---
+++ dev/AI.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+
+import re
+
+
+def alusta_tiedosto():
+
+    tiedosto1 = open("lit.txt", "r", encoding="utf8")
+    tiedosto2 = open("lit2.txt", "w", encoding="utf8")
+
+    for rivi in tiedosto1:
+
+        matchObj = re.match(r'(.*), (.*) - (.*): (.*)\n', rivi)
+
+        if matchObj:
+            teksti = matchObj.group(4)
+            if "<Media jätetty pois>" not in teksti and "https" not in teksti and len(teksti) < 40:
+                tiedosto2.writelines(teksti.lower() + "\n")
+
+
+alusta_tiedosto()
+
+bot = ChatBot("Shaq")
+
+keskustelu = open("lit2.txt", "r", encoding="utf8").readlines()
+
+bot.set_trainer(ListTrainer)
+
+#bot.train(keskustelu)
+
+while True:
+    request = input("Santeri: ")
+    response = bot.get_response(request)
+
+    print("Shaq: ", response)
